4c2e447cc18873674630cbfe1d290504042b87e5
hirakida <hirakida29@gmail.com>
2021-05-01 21:58:43 +0900

    Update

--- http-server/src/main/java/com/example/Main.java
+++ http-server/src/main/java/com/example/Main.java
@@ -3,11 +3,9 @@ package com.example;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-import com.linecorp.armeria.common.HttpRequest;
 import com.linecorp.armeria.common.HttpResponse;
 import com.linecorp.armeria.common.MediaType;
 import com.linecorp.armeria.server.Server;
-import com.linecorp.armeria.server.ServiceRequestContext;
 import com.linecorp.armeria.server.docs.DocService;
 import com.linecorp.armeria.server.logging.AccessLogWriter;
 import com.linecorp.armeria.server.logging.LoggingService;
@@ -20,22 +18,21 @@ public class Main {
                               .serviceUnder("/docs", new DocService())
                               .decorator(LoggingService.newDecorator())
                               .accessLogWriter(AccessLogWriter.combined(), false)
-                              .service("/", (ctx, req) -> HttpResponse.of("Hello, Armeria!"))
-                              .service("/json", Main::json)
-                              .service("/delayed", Main::delayed)
+                              .service("/",
+                                       (ctx, req) -> HttpResponse.of("Hello, Armeria!"))
+                              .service("/json",
+                                       (ctx, req) -> HttpResponse.of(MediaType.JSON_UTF_8,
+                                                                     "{\"datetime\":\"%s\"}",
+                                                                     LocalDateTime.now()))
+                              .service("/delayed",
+                                       (ctx, req) -> {
+                                           HttpResponse response = HttpResponse.of("Hello!");
+                                           return HttpResponse.delayed(response, Duration.ofSeconds(3));
+                                       })
                               .build();
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> server.stop().join()));
 
         server.start().join();
-    }
-
-    private static HttpResponse json(ServiceRequestContext ctx, HttpRequest req) {
-        return HttpResponse.of(MediaType.JSON_UTF_8, "{\"datetime\":\"%s\"}", LocalDateTime.now());
-    }
-
-    private static HttpResponse delayed(ServiceRequestContext ctx, HttpRequest req) {
-        HttpResponse response = HttpResponse.of("delayed");
-        return HttpResponse.delayed(response, Duration.ofSeconds(3));
     }
 }

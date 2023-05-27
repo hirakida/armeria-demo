95de4442f749c5e5f19ee30f7c6d847cb642900b
hirakida <hirakida29@gmail.com>
2023-05-27 16:44:35 +0900

    Update

--- http-server-kotlin/build.gradle.kts
+++ http-server-kotlin/build.gradle.kts
@@ -6,11 +6,10 @@ plugins {
 }
 
 dependencies {
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom"))
+    implementation(platform("org.springframework.boot:spring-boot-dependencies"))
     implementation("com.linecorp.armeria:armeria-kotlin")
     implementation("com.linecorp.armeria:armeria-logback")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-
     testImplementation("com.linecorp.armeria:armeria-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
 //    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")

11cf755eb2fdd8ae2df38e687d1665ea99d188d2
hirakida <hirakida29@gmail.com>
2024-02-04 20:45:06 +0900

    Update

--- websocket/websocket-client/src/main/java/com/example/Main.java
+++ websocket/websocket-client/src/main/java/com/example/Main.java
@@ -17,31 +17,6 @@ public final class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
-        final Subscriber<WebSocketFrame> subscriber = new Subscriber<>() {
-            @Override
-            public void onSubscribe(Subscription subscription) {
-                subscription.request(Long.MAX_VALUE);
-            }
-
-            @Override
-            public void onNext(WebSocketFrame webSocketFrame) {
-                logger.info("type={}", webSocketFrame.type());
-                if (webSocketFrame.type() == WebSocketFrameType.TEXT) {
-                    logger.info("text={}", webSocketFrame.text());
-                }
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                logger.error("error", throwable);
-            }
-
-            @Override
-            public void onComplete() {
-                logger.info("complete");
-            }
-        };
-
         final WebSocketClient client = WebSocketClient.of("ws://localhost:8080");
         client.connect("/ws")
               .thenAccept(session -> {
@@ -49,9 +24,34 @@ public final class Main {
                   session.setOutbound(writer);
                   writer.write("Hello from client");
                   writer.close();
-                  session.inbound().subscribe(subscriber);
+                  session.inbound().subscribe(new SubscriberImpl());
               });
 
-        TimeUnit.SECONDS.sleep(3);
+        TimeUnit.SECONDS.sleep(1);
+    }
+
+    private static class SubscriberImpl implements Subscriber<WebSocketFrame> {
+        @Override
+        public void onSubscribe(Subscription subscription) {
+            subscription.request(Long.MAX_VALUE);
+        }
+
+        @Override
+        public void onNext(WebSocketFrame webSocketFrame) {
+            logger.info("type={}", webSocketFrame.type());
+            if (webSocketFrame.type() == WebSocketFrameType.TEXT) {
+                logger.info("text={}", webSocketFrame.text());
+            }
+        }
+
+        @Override
+        public void onError(Throwable throwable) {
+            logger.error("error", throwable);
+        }
+
+        @Override
+        public void onComplete() {
+            logger.info("complete");
+        }
     }
 }

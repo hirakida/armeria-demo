391fbee98c96716e2e2bdcd0ca4a20c2988ee11c
hirakida <hirakida29@gmail.com>
2024-03-06 19:59:44 +0900

    Update

--- thrift/thrift-client-async/src/main/java/com/example/Main.java
+++ thrift/thrift-client-async/src/main/java/com/example/Main.java
@@ -3,6 +3,9 @@ package com.example;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.example.thrift.CalculatorService;
 import com.example.thrift.Operation;
 import com.example.thrift.Work;
@@ -10,8 +13,11 @@ import com.example.thrift.Work;
 import com.linecorp.armeria.client.logging.LoggingRpcClient;
 import com.linecorp.armeria.client.thrift.ThriftClients;
 import com.linecorp.armeria.common.logging.LogLevel;
+import com.linecorp.armeria.common.thrift.ThriftFuture;
 
 public final class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) throws Exception {
         final CalculatorService.AsyncIface calculatorService =
                 ThriftClients.builder("http://127.0.0.1:8080")
@@ -23,9 +29,13 @@ public final class Main {
                                                            .newDecorator())
                              .build(CalculatorService.AsyncIface.class);
 
-        final AsyncMethodCallbackImpl<Integer> resultHandler = new AsyncMethodCallbackImpl<>();
-        calculatorService.calculate(1, new Work(1, 2, Operation.ADD), resultHandler);
-        calculatorService.calculate(2, new Work(5, 3, Operation.SUBTRACT), resultHandler);
+        final ThriftFuture<Integer> future1 = new ThriftFuture<>();
+        final ThriftFuture<Integer> future2 = new ThriftFuture<>();
+        calculatorService.calculate(1, new Work(1, 2, Operation.ADD), future1);
+        calculatorService.calculate(2, new Work(5, 3, Operation.SUBTRACT), future2);
+        future1.thenAccept(response -> logger.info("{}", response));
+        future2.thenAccept(response -> logger.info("{}", response));
+
         TimeUnit.SECONDS.sleep(1);
     }
 }
